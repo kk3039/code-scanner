30029035aeb5e6a0c9a9a2c85d25321b6d6e4658
Victoria Lu <yinglu95@gmail.com>
2021-03-20 14:12:26 -0400

    parse input and produce ast

---
+++ README.md
@@ -0,0 +1,13 @@
+# NPM Package Code Scanner
+
+## Authors
+
+Cong Yu
+Victoria Lu
+Xiaomeng Wang
+
+## Development
+
+To start locally: `npm run start [input_file_path]`
+args:
+`input_file_path`: path to file which is to be analyzed
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hi there')
+const acorn = require("acorn");
+const fs = require("fs");
+
+const filename = process.argv[2];
+if (!filename) {
+  throw Error("Please provide a file path");
+}
+
+const file = fs.readFileSync(filename, "utf-8");
+
+const parseTree = acorn.parse(file, {
+  ecmaVersion: "latest",
+});
+
+console.log(parseTree);
