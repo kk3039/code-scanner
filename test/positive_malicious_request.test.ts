5dce9b57b4127ffb4820cb7f02c66d4f01d837d1
heliumyc <ericyc4@gmail.com>
2021-03-30 07:36:59 +0800

    finish malicious request finally

--- test/positive_malicious_request.test.ts
+++ test/positive_malicious_request.test.ts
@@ -1,12 +1,19 @@
-import {is_malicious_http_request} from '../src/detect_malicious_request'
+import { is_malicious_http_request } from '../src/detect_malicious_request'
 import fs from 'fs'
 
-describe("This is a simple test", () => {
-  test("Check the sampleFunction function", () => {
-    const buf = fs.readFileSync('./test/sample_code/malicious_request_1.js')
-    const code = buf.toString;
-    console.log(code)
-    expect(is_malicious_http_request("hello")).toBeFalsy()
-  });
-});
+const sample_code_dir = './test/sample_code'
+
+fs.readdirSync(sample_code_dir)
+  .filter(s => s.startsWith('malicious_request'))
+  .filter(s => fs.statSync(`${sample_code_dir}/${s}`).isFile())
+  .forEach(filename => {
+    describe(`[${filename}] should be detected as malicious`, () => {
+      test("is_malicious_request should return true", () => {
+        const buf = fs.readFileSync(`${sample_code_dir}/${filename}`);
+        const code = buf.toString();
+        expect(is_malicious_http_request(code)).toBeTruthy();
+      });
+    });
+})
+
 
